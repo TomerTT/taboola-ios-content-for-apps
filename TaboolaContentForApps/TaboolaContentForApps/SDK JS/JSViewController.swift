cdf5dca34485733a51e4961cd8b7f7baeb75b148
Liad Elidan <liad.e@taboola.com>
2020-03-23 10:45:28 +0200

    js done

--- TaboolaContentForApps/TaboolaContentForApps/SDK JS/JSViewController.swift
+++ TaboolaContentForApps/TaboolaContentForApps/SDK JS/JSViewController.swift
@@ -6,4 +6,36 @@
 //  Copyright © 2020 Liad Elidan. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import WebKit
+import TaboolaSDK
+
+class JSViewController: UIViewController {
+    @IBOutlet weak var webViewContainer: UIView!
+    var webView = WKWebView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.frame = view.frame
+        webViewContainer.addSubview(webView)
+        TaboolaJS.sharedInstance()?.logLevel = .debug
+        TaboolaJS.sharedInstance()?.registerWebView(webView, with: self)
+        // Do any additional setup after loading the view.
+        try? loadExamplePage()
+    }
+    
+    func loadExamplePage() throws {
+        guard let htmlPath = Bundle.main.path(forResource: "JSSamplePage", ofType: "html") else {
+            print("Error loading HTML")
+            return
+        }
+        let appHtml = try String.init(contentsOfFile: htmlPath, encoding: .utf8)
+        webView.loadHTMLString(appHtml, baseURL: URL(string: "https://cdn.taboola.com/mobile-sdk/init/"))
+    }
+}
+
+extension JSViewController: TaboolaJSDelegate {
+    func onItemClick(_ placementName: String!, withItemId itemId: String!, withClickUrl clickUrl: String!, isOrganic organic: Bool) -> Bool {
+        return true
+    }
+}
